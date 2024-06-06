4a9f3e747bc945011e495bdafd5bca7a58317be8
willchrisjr <willchrisjr301@gmail.com>
2024-06-06 04:27:18 -0400

    Added functionality to print page size and number of tables

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 import sys
 
-from dataclasses import dataclass
-
-# import sqlparse - available if you need it!
-
 database_file_path = sys.argv[1]
 command = sys.argv[2]
 
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
-        # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
-
-        # Uncomment this to pass the first stage
+        # Read the database page size
         database_file.seek(16)  # Skip the first 16 bytes of the header
         page_size = int.from_bytes(database_file.read(2), byteorder="big")
         print(f"database page size: {page_size}")
+
+        # Count the number of tables by searching for "CREATE TABLE" in the file
+        database_file.seek(0)  # Go back to the beginning of the file
+        number_of_tables = sum(line.count(b"CREATE TABLE") for line in database_file)
+        print(f"number of tables: {number_of_tables}")
 else:
     print(f"Invalid command: {command}")
