90112a8929509641ffc52cb9e670bad83f181e4e
willchrisjr <willchrisjr301@gmail.com>
2024-06-06 04:08:42 -0400

    Updated files and setup

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import sys
+
+from dataclasses import dataclass
+
+# import sqlparse - available if you need it!
+
+database_file_path = sys.argv[1]
+command = sys.argv[2]
+
+if command == ".dbinfo":
+    with open(database_file_path, "rb") as database_file:
+        # You can use print statements as follows for debugging, they'll be visible when running tests.
+        print("Logs from your program will appear here!")
+
+        # Uncomment this to pass the first stage
+        database_file.seek(16)  # Skip the first 16 bytes of the header
+        page_size = int.from_bytes(database_file.read(2), byteorder="big")
+        print(f"database page size: {page_size}")
+else:
+    print(f"Invalid command: {command}")
